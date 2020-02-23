867a83e630e9d105646630dd9aebf97830f0af2f
BaJaEm DRC <bajaemdrc@gmail.com>
2020-02-23 18:42:27 -0500

    Added example Srping Boot App as WAR

---
+++ src/main/java/org/bajaem/BootWarController.java
@@ -0,0 +1,15 @@
+package org.bajaem;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BootWarController {
+
+	@RequestMapping("/helloWorld")
+	public String helloWorld(Model model) {
+		model.addAttribute("message", "Hello World!");
+		return "helloWorld";
+	}
+}
---
+++ src/test/java/org/bajaem/BootWarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bajaem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootWarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
